2bf8844ccc4bfe3326a2718fcbca407f55b32226
Simon <83026016+simharuk2021@users.noreply.github.com>
2023-02-13 16:00:07 +0000

    email pushed to an array and random image generate

--- js/app.js
+++ js/app.js
@@ -1,16 +1,4 @@
-// const numItemstoGenerate = 1;
-
-// function showItem(){
-//     fetch(`https://source.unsplash.com/1600x900/?random`).then((response) => {
-//         let item = 
-//         document.createElement('div');
-//         item.classList.add('item');
-//         item.innerHTML = `<img src="${response.url}" alt="${response.url}" />`
-//         document.body.appendChild(item);
-//     })
-// }
-
-// showItem();
+var emails = [];
 
 
 
@@ -18,5 +6,67 @@ function randomImg(width, height) {
   
     document.getElementById('img-container').innerHTML = '<img src="https://source.unsplash.com/random/'+width+'x'+height+'">';
   }
+
+
+  const button = document.getElementById('image');
+  const form = document.querySelector('.form');
+  const email = document.getElementById('email');
+
+
+button.addEventListener('click', () => {
+  randomImg(300,300);
+});
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+  
+    validateInputs();
+  });
+  
+
+
+
+
+
+  const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
   
   
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+  }
+  
+  const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+  
+    errorDisplay.innerText = '';
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+  };
+  
+  const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  const validateInputs = () => {
+    const emailValue = email.value.trim();
+
+    if (emailValue === '') {
+      setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+      setError(email, 'Provide a valid email address');
+    } else {
+      setSuccess(email)
+      console.log(emailValue);
+      emails.push(emailValue);
+      console.log(emails);
+   }
+  };
+
+
+
