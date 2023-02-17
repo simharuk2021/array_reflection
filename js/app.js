2d8217342d3f4c66ba3b015930f711ad9b9e1522
Simon <83026016+simharuk2021@users.noreply.github.com>
2023-02-17 15:16:13 +0000

    code comments added and styling changes made

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+// code adapted from https://github.com/name-taken23/JavaScriptArrayReflection
+
+
+
 const src = 'https://picsum.photos/300';
 const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 const storage = [];
@@ -7,6 +11,7 @@ let x = 0;
 let y = 0;
 let z = 0;
 
+// This function requests a new image and stores the image in the url variable
 function newImg() {
     fetch(src).then(response => {
         if (response.status >= 400) {
@@ -23,7 +28,7 @@ function newImg() {
     console.log("Error: " + error));
 }
 
-
+// this function iterates through the emails stored within the array and returns the email 
 function search(email) {
     var result;
 
@@ -32,34 +37,38 @@ function search(email) {
         if (storage[i][0] === email) {
 
             result = storage[i];
-
+          
             break;
         }
-
+    
     }
-
     return result;
 }
 
 
+//  fetches an image when the page loads
 $(window).on('load', newImg);
 $('#genNewImg').on('click', newImg);
 
+// this function stores the email and the url of the image in the array
 $('#assignTo').on('click', () => {
     email = document.forms["formSubmit"]["email"].value;
 
     if (regex.test(email) == false) {
         functionAlert();
     }
+    // checks of the email is already within the array by calling the search function, if false then pushes the email and url to 
+    // the array
     else if (!search(email)) {
 
         storage.push([email, url]);
-        var option = $("<option></option").text(email).attr("value", z=0);
+        var option = $("<option></option").text(email).attr("value", z);
         $('#saved__Emails').append(option);
         newImg();
         z++;
 
-
+// if the result of the search function is true then the email is already in the array and the url is pushed to 
+// the located email address within the array and a new image is fetched
     } else if (search(email)) {
         x = storage.findIndex(e => e.includes(email));
         storage[x].push(url);
@@ -71,10 +80,11 @@ $('#assignTo').on('click', () => {
 });
 
 
-
+// this function updates the saved id with saved emails when the image updates
 $('#saved__Emails').change(updateImg);
 
-
+// function checks through the saved emails and renders the images when the email from the dropdown
+// is selected
 function updateImg() {
     if ($('#saved__Emails').val() === "none") {
         $("img").remove(".items");
@@ -90,6 +100,7 @@ function updateImg() {
     }
 };
 
+// custom invalid e-mail alert
 function functionAlert(msg, myYes) {
     var confirmBox = $("#confirm");
     confirmBox.find(".message").text(msg);
